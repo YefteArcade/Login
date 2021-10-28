e82a08b524bc80357443f19c44cf27295587a3d8
YefteArcade <93025115+YefteArcade@users.noreply.github.com>
2021-10-27 21:57:06 -0700

    Add files via upload

---
+++ LogInApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using LogInApp.Services;
+using LogInApp.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LogInApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
